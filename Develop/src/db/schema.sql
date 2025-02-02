96610f8f137d705dd8efabf16895884ab37f9a4c
Devilwife <nicole.barger22@gmail.com>
2025-02-01 21:34:18 -0500

    fixes on seeds

--- Develop/src/db/schema.sql
+++ Develop/src/db/schema.sql
@@ -5,25 +5,31 @@ CREATE DATABASE employees_db;
 
 CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
-    employee_name VARCHAR(30) UNIQUE NOT NULL
+    sales DECIMAL NOT NULL,
+    legal DECIMAL NOT NULL,
+    finance DECIMAL NOT NULL,
+    engineering DECIMAL NOT NULL,
 );
 
 CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
+    title VARCHAR(100) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
-    FOREIGN KEY (department_id) 
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id),
     REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_name INTEGER NULL
-    FOREIGN KEY (role_id)
+    salary DECIMAL NOT NULL,
+    manager_name INTEGER NULL,
+    FOREIGN KEY (role_id),
     REFERENCES roles(id)
-    
 );
+
+
+
--- Develop/src/db/seed.sql
+++ Develop/src/db/seed.sql
@@ -5,7 +5,7 @@ VALUE (001, 'Serial Primary Key'),
 
 INSERT INTO roles (id, title, salary, department_id) 
 VALUES (001, 'Serial Primary Key', 100000, 001),
-       (002, 'VARCHAR(30) UNIQUE NOT NULL', 80000, 002),
+       (002, 'VARCHAR(100) UNIQUE NOT NULL', 80000, 002),
        (003, 'DECIMAL NOT NULL', 70000, 001),
        (004, 'INTEGER NOT NULL', 60000, 002),
 
@@ -17,3 +17,4 @@ VALUES (001, 'John', 'Doe', '501', 'NULL'),
        (005, 'Chris', 'Brown', '505', 'NULL');
        (006, 'Amanda', 'Jones', '506', 'NULL');
        (007, 'David', 'Davis', '507', 'NULL');
+
