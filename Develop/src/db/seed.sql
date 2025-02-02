861e2d6504934f0292482240f6589eb57eb6bdbe
Devilwife <nicole.barger22@gmail.com>
2025-02-01 21:55:39 -0500

    updates on assignment

--- Develop/src/db/seed.sql
+++ Develop/src/db/seed.sql
@@ -1,20 +1,19 @@
 -- Insert data into tables
 INSERT INTO departments (id, name) 
-VALUE (001, 'Serial Primary Key'),
-      (002, 'VARCHAR(100)'),
+VALUES (1, 'Human Resources'),
+       (2, 'Engineering');
 
 INSERT INTO roles (id, title, salary, department_id) 
-VALUES (001, 'Serial Primary Key', 100000, 001),
-       (002, 'VARCHAR(100) UNIQUE NOT NULL', 80000, 002),
-       (003, 'DECIMAL NOT NULL', 70000, 001),
-       (004, 'INTEGER NOT NULL', 60000, 002),
+VALUES (1, 'Manager', 100000, 1),
+       (2, 'Engineer', 80000, 2),
+       (3, 'Analyst', 70000, 1),
+       (4, 'Technician', 60000, 2);
 
 INSERT INTO employees (id, first_name, last_name, role_id, manager_name)
-VALUES (001, 'John', 'Doe', '501', 'NULL'),
-       (002, 'Jane', 'Smith', '502', 'NULL'),
-       (003, 'Michael', 'Johnson', '503', 'NULL');
-       (004, 'Sara', 'Wilson', '504', 'NULL');
-       (005, 'Chris', 'Brown', '505', 'NULL');
-       (006, 'Amanda', 'Jones', '506', 'NULL');
-       (007, 'David', 'Davis', '507', 'NULL');
-
+VALUES (1, 'John', 'Doe', 1, NULL),
+       (2, 'Jane', 'Smith', 2, NULL),
+       (3, 'Michael', 'Johnson', 3, NULL),
+       (4, 'Sara', 'Wilson', 4, NULL),
+       (5, 'Chris', 'Brown', 1, NULL),
+       (6, 'Amanda', 'Jones', 2, NULL),
+       (7, 'David', 'Davis', 3, NULL);
